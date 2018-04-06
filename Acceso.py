88b924272dab1779454ab0d339d0db6e4f0e4832
JesusPuga <jesuspuga01@gmail.com>
2018-04-06 17:59:26 -0500

    Agregadas pantallas: studentAcces AdministrativeAccess Inscription GroupsInscription
    Se cambió a inglés la declaración de variables y clases, se veía muy pendejo b:

--- Acceso.py
+++ Acceso.py
@@ -3,7 +3,7 @@ import tkinter
 from tkinter import *
 from tkinter import ttk
 
-class Acceso:
+class Access:
     def __init__(self, root):
         self.root = root
         #Se define el nombre de la ventana y se restringe el tamaño de la misma
@@ -45,9 +45,7 @@ class Acceso:
         #Botón Ingresar
         Button(root, text="Ingresar")
 
-
-
-class AccesoAlumno:
+class StudentAccess:
     def __init__(self, root):
         self.root = root
         #Se define el nombre de la ventana y se restringe el tamaño de la misma
@@ -76,6 +74,99 @@ class AccesoAlumno:
         sessionButton = Button(leftFrame, text="Sesión")
         sessionButton.grid(row = 3, column = 0)
 
+class AdministrativeAccess:
+    def __init__(self, root):
+        self.root = root
+        #Se define el nombre de la ventana y se restringe el tamaño de la misma
+        root.title("Sistema de Inscripción | Administrativo")
+        root.geometry('{}x{}'.format(500, 300))
+        root.resizable(0,0)
+        # layout all of the main containers
+        root.grid_rowconfigure(1, weight=1)
+        root.grid_columnconfigure(0, weight=1)
+
+        #Frames a usar, algo así como div b:
+        leftFrame = Frame(root, width=100, height=100)
+        leftFrame.grid(row=0,column=0)
+
+        closeSessionFrame = Frame(root,  bg='gray2',)
+        closeSessionFrame.grid(row=1,column=0)
+
+        rightFrame = Frame(root, bg='lavender', width=400, height=300)
+        rightFrame.grid(row=0,column=1)
+
+        #Botones
+        scheduleButton = Button(leftFrame, text="Horario")
+        scheduleButton.grid(row = 2, column = 0)
+        subjectButton = Button(leftFrame, text="Materias")
+        subjectButton.grid(row = 1, column = 0)
+        sessionButton = Button(leftFrame, text="Sesión")
+        sessionButton.grid(row = 3, column = 0)
+
+class Inscription:
+    def __init__(self, root):
+        self.root = root
+        #Se define el nombre de la ventana y se restringe el tamaño de la misma
+        root.title("Sistema de Inscripción | Inscripción")
+        root.geometry('{}x{}'.format(500, 300))
+        root.resizable(0,0)
+        # layout all of the main containers
+        root.grid_rowconfigure(1, weight=1)
+        root.grid_columnconfigure(0, weight=1)
+
+        #Frames a usar, algo así como div b:
+        topFrame = Frame(root, width=500, height=200)
+        topFrame.grid(row=0,column=0)
+
+        bottomFrame = Frame(root, bg='lavender', width=500, height=100)
+        bottomFrame.grid(row=1,column=0)
+
+        subjectListENY = Entry(topFrame)
+        subjectListENY.grid(row=0, column=0)
+        #selección
+        subjectCveLB = Label(bottomFrame, text="Clave de materia:")
+        subjectCveLB.grid(row=0, column=0)
+        subjectCveENY = Entry(bottomFrame)
+        subjectCveENY.grid(row=0, column=1)
+
+        #Botones
+        selectButton = Button(bottomFrame, text="Seleccionar")
+        selectButton.grid(row = 0, column = 2)
+        returnButton = Button(bottomFrame, text="Regresar")
+        returnButton.grid(row = 1, column = 3)
+
+class GroupsInscription:
+    def __init__(self, root):
+        self.root = root
+        #Se define el nombre de la ventana y se restringe el tamaño de la misma
+        root.title("Sistema de Inscripción | Inscripción de grupos")
+        root.geometry('{}x{}'.format(500, 300))
+        root.resizable(0,0)
+        # layout all of the main containers
+        root.grid_rowconfigure(1, weight=1)
+        root.grid_columnconfigure(0, weight=1)
+
+        #Frames a usar, algo así como div b:
+        topFrame = Frame(root, width=500, height=200)
+        topFrame.grid(row=0,column=0)
+
+        bottomFrame = Frame(root, bg='lavender', width=500, height=100)
+        bottomFrame.grid(row=1,column=0)
+
+        subjectListENY = Entry(topFrame)
+        subjectListENY.grid(row=0, column=0)
+        #selección
+        subjectCveLB = Label(bottomFrame, text="Grupo:")
+        subjectCveLB.grid(row=0, column=0)
+        subjectCveENY = Entry(bottomFrame)
+        subjectCveENY.grid(row=0, column=1)
+
+        #Botones
+        selectButton = Button(bottomFrame, text="Inscribir")
+        selectButton.grid(row = 0, column = 2)
+        returnButton = Button(bottomFrame, text="Regresar")
+        returnButton.grid(row = 1, column = 3)
+
 
 if __name__ == '__main__':
     """ CREACIÓN DE LA VENTANA DE ACCESO AL SISTEMA"""
@@ -83,7 +174,7 @@ if __name__ == '__main__':
     #Declara ventana de aplicación
     root = Tk()
 
-    aplicacion = AccesoAlumno(root)# prueba de nueva ventana
+    aplicacion = GroupsInscription(root)# prueba de nueva ventana
 
     #Bucle de la aplicación
     root.mainloop()
