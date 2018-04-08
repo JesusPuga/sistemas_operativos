b55f088098b53676b36d2ac0d493c3b6a9a22839
JesusPuga <jesuspuga01@gmail.com>
2018-04-08 01:08:29 -0500

    Agregada función de validar usuario

--- Validaciones.py
+++ Validaciones.py
@@ -4,8 +4,29 @@ from BDConexion import *
 con = Conexion("root","run maria run","FacultadBD")
 
 def student_inf():
-    query_p= "SELECT * FROM Alumno"
-    con.execute_query(query_p)
+    query = "SELECT * FROM Alumno"
+    con.execute_query(query)
 
+def validate_user(type,user, password):
 
+    if type != "Alumnos" and type != "Administrativos":
+        return "Tipo de usuario inexistente"
+
+    query = """SELECT carnetUsuario, tipoUsuario
+               FROM Usuario
+               WHERE carnetUsuario = %s and tipoUsuario = %s"""
+    result = con.execute_query(query, (user,type))
+
+    if result == None:
+        return "Usuario equivocado"
+
+    query = """SELECT carnetUsuario, tipoUsuario, contrasenia
+               FROM Usuario
+               WHERE contrasenia = %s and tipoUsuario = %s and carnetUsuario= %s"""
+    result = con.execute_query(query, (password, type, user))
+    print(result)
+    if result == None:
+        return "Contraseña equivocada"
+
+print(validate_user("Alumnos","user","password"))
 student_inf()
